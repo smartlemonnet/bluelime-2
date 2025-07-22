2c4fcd049fa285b578c00a5c1cac31126da72826
smartlemonnet <smartlemon.net@gmail.com>
2025-07-22 13:17:18 +0200

    Delete src1 directory

--- src1/lib/customSupabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://szkxnbzhgzlnutlmuvrf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6a3huYnpoZ3psbnV0bG11dnJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyODI4MDIsImV4cCI6MjA2NTg1ODgwMn0.c1Trc1kHEt-oO98ryXBsXLaqgiHTmRiIxTIPYy-7TPI';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
